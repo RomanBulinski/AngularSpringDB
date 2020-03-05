c40a70f1b3c0f11cae1efced9e93da60b5156cff
rbulin <roman.bulinski@pentacomp.pl>
2020-03-05 20:06:20 +0100

    Created example with Cities

---
+++ src/main/java/com/buulean/angularspringdb/service/ICityService.java
@@ -0,0 +1,10 @@
+package com.buulean.angularspringdb.service;
+
+import com.buulean.angularspringdb.model.City;
+
+import java.util.List;
+
+public interface ICityService {
+
+    List<City> findAll();
+}
